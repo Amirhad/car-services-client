b4549f6429c83dcf4ea2a9791c4f85bc98f84447
Bilal <obkh1309@mail.ru>
2022-01-05 15:43:55 +0300

    redux pal

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,14 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger/src';
-
+import {authentication} from './features/authentication';
+import {carService} from './features/service';
 
 const logger = createLogger({
   diff: true,
   collapsed: true
 })
 
-export const store = createStore(applyMiddleware(thunk, logger))
+const combineReducer = combineReducers({authentication,carService})
+
+export const store = createStore(combineReducer,applyMiddleware(thunk, logger))
