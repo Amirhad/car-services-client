d2cebb5c56b590fa485be6e0d5a363152c0a40fc
Iznaur18 <gimer8@mail.ru>
2022-01-05 16:54:37 +0300

    header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./header.module.css";
+
+const Header = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.header__main}>
+          <div className={styles.header__row}>
+            <div>О нас</div>
+            <div>Контакты</div>
+            <div>FAQ</div>
+            <div>Карта</div>
+          </div>
+        </div>
+        <div className={styles.header__title}>
+          <h1>Подбирем автосервис для решения проблемы вашего авто</h1>
+          <button className={styles.button-23}>список автосервисов</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
