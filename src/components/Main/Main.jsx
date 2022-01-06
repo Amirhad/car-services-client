28001e77ed36b0ca2d97a99e48e892c6fe87c081
Amir <amir.95mail@gmail.com>
2022-01-06 17:23:27 +0300

    amir commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import Cart from "../Carts";
-import style from "./main.module.css";
+
 
 const Main = () => {
   return (
     <>
-      <div className={style.main__serch__form}>
-        <h1 className={style.main__serch__form__title}>Поиск автосервиса</h1>
-        <div><input className={style.main__serch__form__input} type="text" placeholder="найти автосервис" /></div>
-      </div>
-
-      <Cart />
-    </>
+    <Cart/>
+      </>
   );
 };
 
 export default Main;
+
+
+
+
