5ca6184794a40c0730a7eeaa0833fa0ebf4af1df
adam <mutaev@mail.ru>
2022-01-06 17:23:15 +0300

    ok

--- src/components/Sign/SignIn.jsx
+++ src/components/Sign/SignIn.jsx
@@ -1,14 +1,28 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from './sign.module.css'
+import {useDispatch, useSelector} from 'react-redux'
+// import auth from '../../redux/features/authentication'
 
 function SignIn() {
+  const dispatch = useDispatch()
+  const signIn = useSelector(state => state.authentication.signin)
+
   const [login, setLogin] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate()
 
+  const handleSubmit = () => {
+    // dispatch(auth(login, password))
+  }
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    navigate('/')
+  }
   return (
 
-    <div className={styles.form}>
+    <form className={styles.form} onSubmit={handleLogin}>
       <div className={styles.block}>
         <h2>Авторизация</h2>
         <div>
@@ -28,12 +42,16 @@ function SignIn() {
           />
         </div>
         <div>
-          <button className={styles.btn}>Войти</button>
+          <button 
+          className={styles.btn}
+          disabled={signIn}
+          onClick={handleSubmit}
+          >Войти</button>
         </div>
         <span>У вас нет акаунта?</span>
         <Link to='/signup'>Регистрация</Link>
       </div>
-    </div>
+    </form>
 
   );
 }
--- src/components/Sign/SignInUp.jsx
+++ src/components/Sign/SignInUp.jsx
@@ -1,21 +1,26 @@
 import { createService } from '../../redux/features/authentication'
 import { useState } from 'react';
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import styles from './sign.module.css'
 import { Link, useNavigate } from 'react-router-dom';
 
 function SignInUp() {
 
-  // const signinUp = useSelector(state => state.authentication.signingUp)
+  const signinUp = useSelector(state => state.authentication.signingUp)
 
   const dispatch = useDispatch()
   const navigate = useNavigate()
+  const [toLogin, setToLogin] = useState('')
+  const [name, setName] = useState('')
+  const [city, setCity] = useState('')
+  const [street, setStreet] = useState('')
   const [login, setLogin] = useState('')
   const [password, setPassword] = useState('')
+  const [img, setImg] = useState('')
 
 
   const handleSubmit = () => {
-    dispatch(createService(login, password))
+    dispatch(createService(login, password, toLogin, name, city, street, img))
   }
   const handleLogin = (e) => {
     e.preventDefault()
@@ -23,54 +28,66 @@ function SignInUp() {
   }
 
   return (
-    <form className={styles.form} onSubmit={handleLogin}>
-      <div className={styles.block}>
-        <h2>Авторизация</h2>
-        <div>
-          <input
-            type="text"
-            placeholder="Name" />
-          <input 
-          type="text" 
-          placeholder='login'/>
+    <>
+      <form className={styles.form} onSubmit={handleLogin}>
+        <div className={styles.block}>
+          <h2>Авторизация</h2>
+          <div>
+            <input
+              type="text"
+              value={name}
+              placeholder="Name"
+              onChange={(e) => setName(e.target.value)} />
+            <input
+              type="text"
+              value={toLogin}
+              placeholder='login'
+              onChange={(e) => setToLogin(e.target.value)} />
+          </div>
+          <div>
+            <input
+              type="text"
+              value={login}
+              placeholder="Введите email"
+              onChange={(e) => setLogin(e.target.value)}
+            />
+            <input
+              type="text"
+              value={password}
+              placeholder="Введите пароль"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              value={city}
+              placeholder='city'
+              onChange={(e) => setCity(e.target.value)} />
+            <input
+              type="text"
+              value={street}
+              placeholder='street'
+              onChange={(e) => setStreet(e.target.value)} />
+          </div>
+          <div>
+            <input type="file"
+            value={img}
+            onChange={(e) => setImg(e.target.value)}/>
+          </div>
+          <div>
+            <button
+              className={styles.btn}
+              disabled={signinUp}
+              onClick={handleSubmit}>
+              Регистрация
+            </button>
+          </div>
+          <span>Уже есть аккаунт?</span>
+          <Link to='/signin'>Войти</Link>
         </div>
-        <div>
-          <input
-            type="text"
-            value={login}
-            placeholder="Введите email"
-            onChange={(e) => setLogin(e.target.value)}
-          />
-          <input
-            type="text"
-            value={password}
-            placeholder="Введите пароль"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div>
-          <input 
-          type="text" 
-          placeholder='city'/>
-          <input 
-          type="text" 
-          placeholder='street'/>
-        </div>
-        <div>
-          <input type="file" />
-        </div>
-        <div>
-          <button
-            className={styles.btn}
-            // disabled={signinUp}
-            onClick={handleSubmit}>
-            Регистрация
-          </button>
-        </div>
-        <span>Уже есть аккаунт?</span>
-        <Link to='/signin'>Войти</Link>
-      </div>
-    </form>
+      </form>
+    </>
   );
 }
 
