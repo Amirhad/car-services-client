0e99020de9b53bee1e8a3bee0333c9b7c0c577cd
Amir <amir.95mail@gmail.com>
2022-01-06 13:28:15 +0300

    asd

--- src/components/Carts/index.jsx
+++ src/components/Carts/index.jsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector,useDispatch } from "react-redux";
+import { loadCarServices } from "../../redux/features/carService";
 import styles from "./carts.module.css";
 function Cart() {
+
+  const dispatch = useDispatch()
+
+
+  useEffect(() =>{
+    dispatch(loadCarServices())
+  }, [dispatch])
+  const services = useSelector(state => state.carService.carServices)
   return (
-    <div>
-      <div className={styles.carts__container}>
+     
+          
+      <div className={styles.carts__container} >
+         {services.map((item) => {
+        return(
         <div className={styles.cart__body}>
           <div className={styles.cart__imageDiv}>
             <img
               className={styles.cart__image}
-              src="http://gmural.ru/wp-content/uploads/2015/10/main-slide-3.jpg"
+              src={item.img}
               alt="service"
             />
           </div>
           <div className={styles.cart__text}>
-            <div><p>ул.Грешева 7 | 7-989-222-33-44</p></div>
+            <div><p>г.{item.address.city} ул.{item.address.street} {item.address.number} | {item.phone}</p></div>
             <a className={styles.button1} href="/">открыть</a>
           </div>
         </div>
-      </div>
-    </div>
+     
+        )
+      })}</div>
+    
+   
   );
 }
 
