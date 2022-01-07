30d4704518e3da4cdf44a6dc74660ae1844834e7
adam <mutaev@mail.ru>
2022-01-07 14:38:45 +0300

    Добавление токена в авторизацию

--- src/components/Sign/SignIn.jsx
+++ src/components/Sign/SignIn.jsx
@@ -3,10 +3,12 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styles from './sign.module.css'
 import { useDispatch, useSelector } from 'react-redux'
+import TextField from '@mui/material/TextField';
 
 function SignIn() {
   const signIn = useSelector(state => state.authentication.signin)
   const error = useSelector(state => state.authentication.error)
+  const token = useSelector(state => state.authentication.token)
   const dispatch = useDispatch()
 
   const [tologin, setToLogin] = useState("");
@@ -19,43 +21,51 @@ function SignIn() {
 
   const handleLogin = (e) => {
     e.preventDefault()
+  }
+
+  if (token) {
     navigate('/')
   }
-  return (
+    return (
+      <form className={styles.form} onSubmit={handleLogin}>
+        <div className={styles.block}>
+          {error}
+          <h2>Авторизация</h2>
+          <div>
+            <TextField
+              id="standard-basic" label="Email" variant="standard"
+              type="text"
+              value={tologin}
+              placeholder="Введите Email"
+              onChange={(e) => setToLogin(e.target.value)}
+            />
+          </div>
+          <div>
+            <TextField
+              id="standard-basic" label="password" variant="standard"
+              type="password"
+              value={password}
+              placeholder="Введите пароль"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <button
+              className={styles.btn}
+              disabled={signIn}
+              onClick={handleSubmit}
+            >Войти</button>
+          </div>
+          <span>У вас нет акаунта?</span>
+          <Link to='/signup'>Регистрация</Link>
+          <div className={styles.homeButton}>
+            <Link to='/'>Главное меню</Link>
+          </div>
+        </div>
+      </form>
 
-    <form className={styles.form} onSubmit={handleLogin}>
-      <div className={styles.block}>
-        {error}
-        <h2>Авторизация</h2>
-        <div>
-          <input
-            type="text"
-            value={tologin}
-            placeholder="Введите email"
-            onChange={(e) => setToLogin(e.target.value)}
-          />
-        </div>
-        <div>
-          <input
-            type="password"
-            value={password}
-            placeholder="Введите пароль"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div>
-          <button
-            className={styles.btn}
-            disabled={signIn}
-            onClick={handleSubmit}
-          >Войти</button>
-        </div>
-        <span>У вас нет акаунта?</span>
-        <Link to='/signup'>Регистрация</Link>
-      </div>
-    </form>
+    );
 
-  );
 }
 
 export default SignIn;
