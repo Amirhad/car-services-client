3020f94eb3bc21926b94cc698e00ea448499aee5
Ramzan <ramzan4ik95@mail.ru>
2022-01-09 12:41:10 +0300

    изменен редюсер и некоторые компоненты

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,35 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { logOut } from "../../redux/features/authentication";
 import styles from "./header.module.css";
 
 const Header = () => {
+  const token = useSelector((state) => state.authentication.token);
+  const dispatch = useDispatch();
+
+  const handleClickLogout = () => {
+    dispatch(logOut());
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <div className={styles.header__main}>
           <div className={styles.header__row}>
-            <Link to='/signin' className={styles.signin}>
-              войти
-            </Link>
+            {!token ? (
+              <button className={styles.signin}>
+                <Link to="/signin">Войти</Link>
+              </button>
+            ) : (
+              <button 
+              className={styles.signin} 
+              onClick={handleClickLogout}
+              > 
+              Выйти 
+              </button>
+            )}
+
             <Link to="/about">О нас</Link>
             <Link to="/contacts">Контакты</Link>
             <Link to="/faq">FAQ</Link>
