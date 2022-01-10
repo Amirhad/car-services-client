aba16a7c9d214b0b1a735a7f2800cd548b58687e
adam <mutaev@mail.ru>
2022-01-10 10:48:22 +0300

    изменение стилей

--- src/components/Sign/SignIn.jsx
+++ src/components/Sign/SignIn.jsx
@@ -1,71 +1,72 @@
-import { logIn } from '../../redux/features/authentication'
+import { logIn } from "../../redux/features/authentication";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import styles from './sign.module.css'
-import { useDispatch, useSelector } from 'react-redux'
-import TextField from '@mui/material/TextField';
+import styles from "./sign.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import TextField from "@mui/material/TextField";
 
 function SignIn() {
-  const signIn = useSelector(state => state.authentication.signin)
-  const error = useSelector(state => state.authentication.error)
-  const token = useSelector(state => state.authentication.token)
-  const dispatch = useDispatch()
+  const signIn = useSelector((state) => state.authentication.signin);
+  const error = useSelector((state) => state.authentication.error);
+  const token = useSelector((state) => state.authentication.token);
+  const dispatch = useDispatch();
 
   const [tologin, setToLogin] = useState("");
   const [password, setPassword] = useState("");
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleSubmit = () => {
-    dispatch(logIn(tologin, password))
-  }
+    dispatch(logIn(tologin, password));
+  };
 
   const handleLogin = (e) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+  };
 
-  if (token) {
-    navigate('/')
-  }
-    return (
-      <form className={styles.form} onSubmit={handleLogin}>
-        <div className={styles.block}>
-          {error}
-          <h2>Авторизация</h2>
-          <div>
-            <TextField
-              id="standard-basic" label="Email" variant="standard"
-              type="text"
-              value={tologin}
-              placeholder="Введите Email"
-              onChange={(e) => setToLogin(e.target.value)}
-            />
-          </div>
-          <div>
-            <TextField
-              id="standard-basic" label="password" variant="standard"
-              type="password"
-              value={password}
-              placeholder="Введите пароль"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div>
-            <button
-              className={styles.btn}
-              disabled={signIn}
-              onClick={handleSubmit}
-            >Войти</button>
-          </div>
-          <span>У вас нет акаунта?</span>
-          <Link to='/signup'>Регистрация</Link>
-          <div className={styles.homeButton}>
-            <Link to='/'>Главное меню</Link>
-          </div>
+  return (
+    <form className={styles.form} onSubmit={handleLogin}>
+      <div className={styles.block}>
+        <h2>Авторизация</h2>
+        <h3 className={styles.error__authorization}>{error}</h3>
+        <div>
+          <TextField
+            id="standard-basic"
+            label="Login"
+            variant="standard"
+            type="text"
+            value={tologin}
+            placeholder="Введите логин"
+            onChange={(e) => setToLogin(e.target.value)}
+          />
         </div>
-      </form>
-
-    );
-
+        <div>
+          <TextField
+            id="standard-basic"
+            label="password"
+            variant="standard"
+            type="password"
+            value={password}
+            placeholder="Введите пароль"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <button
+            className={styles.btn}
+            disabled={signIn}
+            onClick={handleSubmit}
+          >
+            Войти
+          </button>
+        </div>
+        <span>У вас нет акаунта?</span>
+        <Link to="/signup">Регистрация</Link>
+        <div className={styles.homeButton}>
+          <Link to="/">Главное меню</Link>
+        </div>
+      </div>
+    </form>
+  );
 }
 
 export default SignIn;
--- src/components/Sign/SignInUp.jsx
+++ src/components/Sign/SignInUp.jsx
@@ -1,131 +1,161 @@
-import { createService } from '../../redux/features/authentication'
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
-import styles from './sign.module.css'
-import { Link, useNavigate } from 'react-router-dom';
-import TextField from '@mui/material/TextField';
+import { createService } from "../../redux/features/authentication";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import styles from "./sign.module.css";
+import { Link, useNavigate } from "react-router-dom";
+import TextField from "@mui/material/TextField";
 
 function SignInUp() {
+  const signinUp = useSelector((state) => state.authentication.signingUp);
 
-  const signinUp = useSelector(state => state.authentication.signingUp)
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-  
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [login, setLogin] = useState('')
-  const [name, setName] = useState('')
-  const [city, setCity] = useState('')
-  const [street, setStreet] = useState('')
-  const [number, setNumber] = useState('')
-  const [phone, setPhone] = useState('')
-  const [text, setText] = useState('')
-  const [img, setImg] = useState('')
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [login, setLogin] = useState("");
+  const [name, setName] = useState("");
+  const [city, setCity] = useState("");
+  const [street, setStreet] = useState("");
+  const [number, setNumber] = useState("");
+  const [phone, setPhone] = useState("");
+  const [text, setText] = useState("");
+  
 
   const handleSubmit = () => {
-    dispatch(createService(email, password, login, name, city, street, number, phone, text, img))
-    navigate('/signin')
-    
-  }
+    dispatch(
+      createService(
+        email,
+        password,
+        login,
+        name,
+        city,
+        street,
+        number,
+        phone,
+        text,
+        
+      )
+    );
+    navigate("/signin");
+  };
   const handleLogin = (e) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+  };
 
+ 
   return (
-      <form className={styles.form} onSubmit={handleLogin}>
-        <div className={styles.block}>
-          <h1>Регистрация</h1>
-          <div>
-            <input
-            value={img}
-            onChange={(e) => setImg(e.target.value)}
-            type="file" />
-          </div>
-          <div className={styles.inputTop}>
-            <TextField
-              id="standard-basic" label="name" variant="standard"
-              type="text"
-              value={name}
-              placeholder="Name"
-              onChange={(e) => setName(e.target.value)} />
-            <TextField
-              id="standard-basic" label="login" variant="standard"
-              type="text"
-              value={login}
-              placeholder='login'
-              onChange={(e) => setLogin(e.target.value)} />
-          </div>
-          <div>
-            <TextField
-              id="standard-basic" label="Email" variant="standard"
-              type="text"
-              value={email}
-              placeholder="Введите email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-              id="standard-basic" label="password" variant="standard"
-              type="password"
-              value={password}
-              placeholder="Введите пароль"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div>
-            <h2>Адрес</h2>
-            <TextField
-              id="standard-basic" label="city" variant="standard"
-              type="text"
-              value={city}
-              placeholder='city'
-              onChange={(e) => setCity(e.target.value)} />
-            <TextField
-              id="standard-basic" label="street" variant="standard"
-              type="text"
-              value={street}
-              placeholder='street'
-              onChange={(e) => setStreet(e.target.value)} />
-            <TextField
-              id="standard-basic" label="number" variant="standard"
-              type="text"
-              value={number}
-              placeholder='number'
-              onChange={(e) => setNumber(e.target.value)} />
-          </div>
-          <div>
-            <h2>Номер телефона</h2>
-            <TextField
-              id="standard-basic" label="phone" variant="standard"
-              type="tel"
-              value={phone}
-              placeholder='phone'
-              onChange={(e) => setPhone(e.target.value)} />
-          </div>
-          <div>
-            <textarea
-              className={styles.textarea}
-              value={text}
-              onChange={(e) => setText(e.target.value)}
-              name=""
-              id=""
-              cols="30"
-              rows="10">напишите текст</textarea>
-          </div>
-          <div>
-            <button
-              className={styles.btn}
-              disabled={signinUp}
-              onClick={handleSubmit}>
-              Регистрация
-            </button>
-          </div>
-          <span>Уже есть аккаунт?</span>
-          <Link to='/signin'>Войти</Link>
+    <div className={styles.form} onSubmit={handleLogin}>
+      <div className={styles.block}>
+        <h1>Регистрация</h1>
+        <div className={styles.inputTop}>
+          <TextField
+            id="standard-basic"
+            label="name"
+            variant="standard"
+            type="text"
+            value={name}
+            placeholder="Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            id="standard-basic"
+            label="login"
+            variant="standard"
+            type="text"
+            value={login}
+            placeholder="login"
+            onChange={(e) => setLogin(e.target.value)}
+          />
         </div>
-      </form>
+        <div>
+          <TextField
+            id="standard-basic"
+            label="Email"
+            variant="standard"
+            type="text"
+            value={email}
+            placeholder="Введите email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            id="standard-basic"
+            label="password"
+            variant="standard"
+            type="password"
+            value={password}
+            placeholder="Введите пароль"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <h2>Адрес</h2>
+          <TextField
+            id="standard-basic"
+            label="city"
+            variant="standard"
+            type="text"
+            value={city}
+            placeholder="city"
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <TextField
+            id="standard-basic"
+            label="street"
+            variant="standard"
+            type="text"
+            value={street}
+            placeholder="street"
+            onChange={(e) => setStreet(e.target.value)}
+          />
+          <TextField
+            id="standard-basic"
+            label="number"
+            variant="standard"
+            type="text"
+            value={number}
+            placeholder="number"
+            onChange={(e) => setNumber(e.target.value)}
+          />
+        </div>
+        <div>
+          <h2>Номер телефона</h2>
+          <TextField
+            id="standard-basic"
+            label="phone"
+            variant="standard"
+            type="tel"
+            value={phone}
+            placeholder="phone"
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </div>
+        <div>
+          <textarea
+            className={styles.textarea}
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            name=""
+            id=""
+            cols="30"
+            placeholder="text"
+            rows="10"
+          />
+        </div>
+        <div>
+          <button
+            className={styles.btn}
+            disabled={signinUp}
+            onClick={handleSubmit}
+          >
+            Регистрация
+          </button>
+        </div>
+        <span>Уже есть аккаунт?</span>
+        <Link to="/signin">Войти</Link>
+      </div>
+    </div>
   );
 }
 
-export default SignInUp;
+export default SignInUp
