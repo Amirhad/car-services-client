16793e13d3e204c1b8928d4c9752dcf5ed6f3c47
Iznaur18 <gimer8@mail.ru>
2022-01-11 12:59:29 +0300

    сделал свайп в faq

--- src/components/Header/links/Faq.jsx
+++ src/components/Header/links/Faq.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import Scrollable from "./scrollableFolder/scrollable";
+import "./linksStyles/faq.css";
 
 const Faq = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default Faq
+  const items = [
+    {
+      modal1: false,
+      title: "Что такое замыкание?",
+      text: "text",
+    },
+    {
+      title: "Заголовок 2",
+      text: "text",
+    },
+    {
+      title: "Заголовок 3",
+      text: "text",
+    },
+    {
+      title: "Заголовок 4",
+      text: "text",
+    },
+    {
+      title: "Заголовок 5",
+      text: "text",
+    },
+    {
+      title: "Заголовок 6",
+      text: "text",
+    },
+    {
+      title: "Заголовок 7",
+      text: "text",
+    },
+    {
+      title: "Заголовок 8",
+      text: "text",
+    },
+  ]
+
+  return (
+    <>
+      <div className="container">
+        <Scrollable _class="items">
+          {items.map((item) => {
+            return (
+              <div className="item">
+                <h2>{item.title}</h2>
+                <p>{item.text}</p>
+              </div>
+            );
+          })}
+        </Scrollable>
+      </div>
+    </>
+  );
+};
+
+export default Faq;
