b08993b8888648954e026b9d4feb9ece94d193a7
Iznaur18 <gimer8@mail.ru>
2022-01-12 17:11:53 +0300

    удалил все в файле фак

--- src/components/Header/links/Faq.jsx
+++ src/components/Header/links/Faq.jsx
@@ -1,60 +1,11 @@
-import Scrollable from "./scrollableFolder/scrollable";
-import "./linksStyles/faq.css";
+import React from 'react'
 
 const Faq = () => {
-
-  const items = [
-    {
-      modal1: false,
-      title: "Что такое замыкание?",
-      text: "text",
-    },
-    {
-      title: "Заголовок 2",
-      text: "text",
-    },
-    {
-      title: "Заголовок 3",
-      text: "text",
-    },
-    {
-      title: "Заголовок 4",
-      text: "text",
-    },
-    {
-      title: "Заголовок 5",
-      text: "text",
-    },
-    {
-      title: "Заголовок 6",
-      text: "text",
-    },
-    {
-      title: "Заголовок 7",
-      text: "text",
-    },
-    {
-      title: "Заголовок 8",
-      text: "text",
-    },
-  ]
-
   return (
-    <>
-      <div className="container">
-        <Scrollable _class="items">
-          {items.map((item) => {
-            return (
-              <div className="item">
-                <h2>{item.title}</h2>
-                <p>{item.text}</p>
-              </div>
-            );
-          })}
-        </Scrollable>
-      </div>
-    </>
-  );
-};
+    <div>
+      что то будет
+    </div>
+  )
+}
 
-export default Faq;
+export default Faq
