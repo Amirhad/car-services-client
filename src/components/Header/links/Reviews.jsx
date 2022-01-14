225c1ac42aa14f44932e7c38dd15a4b688475375
Ramzan <ramzan4ik95@mail.ru>
2022-01-14 14:21:55 +0300

    убрал тег -а- и добавил NavLink

--- src/components/Header/links/Reviews.jsx
+++ src/components/Header/links/Reviews.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import SnakeGame from './game/SnakeGame'
+import { NavLink } from 'react-router-dom'
 import './linksStyles/game.css'
 
 const Reviews = () => {
   return (
     <div>
       <SnakeGame />
-      <button className='button'><a style={{color: "black"}} href="/">←Назад</a></button>
+      <button className='button'><NavLink style={{color: "black"}} to="/">←Назад</NavLink></button>
     </div>
   )
 }
